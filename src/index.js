d46b29b74ee03ca5003108f1ac0f0a2ccfd9e402
Gelo Chang <ayrosochang16@gmail.com>
2020-12-16 19:07:36 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  // uri: 'http://localhost:4000/graphql',
+  uri: 'https://random-color-api.herokuapp.com/graphql',
   cache: new InMemoryCache()
 })
 
